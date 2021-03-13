b3f687d354e6d2c57b2bb4bc2d261b8d44139fc5
Faralaks <Faralaks@ya.ru>
2021-03-13 14:50:17 +0300

    Добавлена визуализация для неотвеченных вопросов бланка

--- html_generator/html_generator.py
+++ html_generator/html_generator.py
@@ -5,13 +5,12 @@ ans = open("answers.txt").read().split("\n")
 results_file = open("result.txt", "w")
 
 big_template = """
-    <div class="card mb-5" id="card{}">
-        <div class="card-header">Вопрос {}</div>
-        <div class="card-body">
+    <div class="card mb-5 questionCard" id="card{}">
+        <div class="card-header">Вопрос {} <i class="fa fa-check pull-right dn" aria-hidden="true" style="color: green" id="checkSign{}"></i></div>
+        <div class="card-body questionBody">
             <h5 class="card-title">{}</h5>
 
 {}
-            
         </div>
     </div>
 
@@ -19,7 +18,7 @@ big_template = """
 
 
 little_template = """            <div class="form-check">
-                <input id="{}" class="form-check-input" type="radio" name="{}"  value="{}">
+                <input id="{}" class="form-check-input" type="radio" name="{}"  value="{}" onchange="$(checkSign{}).show()">
                 <label for="{}" class="form-check-label">{}</label>
             </div>"""
 
@@ -27,9 +26,9 @@ for i, q in enumerate(qests, 1):
     variants = ""
     for j, a in enumerate(ans, 1):
         q_id = "q%da%d" % (i, j)
-        variant = little_template.format(q_id, "q%d"%i, j, q_id, a).replace("\n\n", "") + "\n"
+        variant = little_template.format(q_id, "q%d"%i, j, i, q_id, a).replace("\n\n", "") + "\n"
         variants += variant
-    qestion = big_template.format(i, i, q, variants)
+    qestion = big_template.format(i, i, i, q, variants)
     results_file.write(qestion)
 
 
